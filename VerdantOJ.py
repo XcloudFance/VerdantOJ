2b73f66b2610456b037146e9425d2d64f8f2eda8
XCloud <34064977+XcloudFance@users.noreply.github.com>
2019-09-10 10:34:08 +0800

    fix  a bug
    file io是新增功能，还有很严重的问题
    新增加的错误判断
    预计下个版本推出flask后端+html前端版本
    预计自带用户系统

--- VerdantOJ.py
+++ VerdantOJ.py
@@ -1,17 +1,20 @@
 #/usr/bin/env python
-#coding=utf-8
-#进程检测程序
+#-*- coding = utf-8 -*-
+
 import psutil,demjson,os,time
 from subprocess import PIPE
 from compiler import *
 def getfile(name):
-	f = open(name,'r')
+	f = open(name,'r',encoding='utf-8')
 	res = f.read()
 	f.close()
 	return res
-testcode = input('请输入题号：')
+testcode = input('input titlenum:')
 config = demjson.decode(getfile(testcode+'/config.json'))
-print ('测评题为:',config['title'])
+mem = 0
+timer = 0
+end = 0
+print ('title : ',config['title'])
 if config['IO'] == 'standard IO':
 	count = config['datacount']
 	if os.path.exists('run.exe'):
@@ -25,18 +28,21 @@ if config['IO'] == 'standard IO':
 	for i in range(0,int(count)):
 		data = config['configuration'][str(i)]
 		infile = getfile(testcode+'/'+data['in'])
-		outfile = getfile(testcode+'/'+data['out']).strip()#读取完out文件自动去除空格前缀and后缀
+		outfile = getfile(testcode+'/'+data['out']).replace('\n',' ').strip()
 		start = time.clock()
-		p = psutil.Popen(['run.exe'], stdout=PIPE, stdin=PIPE)
-		p.stdin.write(str(infile+'\r\n').encode('GBK'))
-		p.stdin.flush()
-		mem = p.memory_info().rss
-		out = p.stdout.read().strip()
-		end = time.clock()
-		ret = p.wait()#wait相当于call的返回值，csdn:https://www.cnblogs.com/zhoug2020/p/5079407.html
-		mem = int(mem) / 1024 #放到这里计算是为了防止时间被算的太多，当然也可以分段取时间
-		timer = int(data['timelimit'])/1000
-		print('data'+str(i)+' time:',end-start,'memory:',mem,'in:',infile,'yourout:',out.decode(),'correctout:',outfile)
+		try:
+                        p = psutil.Popen(['run.exe'], stdout=PIPE, stdin=PIPE)
+                        p.stdin.write(str(infile+'\r\n').encode('utf-8'))
+                        p.stdin.flush()
+                        mem = p.memory_info().vms
+                        out = p.stdout.read().decode().replace('\n',' ').strip()
+                        end = time.clock()
+                        ret = p.wait()
+                        mem = int(mem) / 1024 /1024#
+                        timer = int(data['timelimit'])/1000
+                        
+		except:ret=1;
+		print('data'+str(i)+' time:',end-start,'memory:',mem)
 		if ret!=0:
 			print('Result:Runtime Error')
 			continue
@@ -46,9 +52,54 @@ if config['IO'] == 'standard IO':
 		if mem >= int(data['memory']):
 			print('Result:Memory Limit Exceeded')
 			continue
-		if out.decode() != outfile:
+		if out != outfile:
 			print('Result:Wrong Anwser')
 			continue
 		print('Result:Accepted')
 		tot+=int(data['score'])
+elif config['IO'] == 'File IO':
+	count = config['datacount']
+	filename = config['ioname']
+	if os.path.exists('run.exe'):
+		os.remove('run.exe')
+	compiling('c++','run.cpp','run.exe')
+	if not os.path.exists('run.exe'):
+		print('Result:Compile Error')
+		print('Total Score:', 0)
+		exit(0)
+	tot = 0
+	for i in range(0,int(count)):
+                data = config['configuration'][str(i)]
+                infile = getfile(testcode+'/'+data['in'])
+                outfile = getfile(testcode+'/'+data['out']).replace('\n',' ').strip()
+                fs = open(filename+'.in','w+')
+                fs.write(infile)
+                fs.close()
+                start = time.clock()
+                #try:
+                p = psutil.Popen('run.exe',shell=True)
+                yourout = getfile(filename+'.out').strip()
+                
+                mem = p.memory_info().vms
+                end = time.clock()
+                ret = p.wait()
+                mem = int(mem) / 1024 /1024
+                timer = int(data['timelimit'])/1000
+                        
+                #except:ret=1
+                print('data'+str(i)+' time:',end-start,'memory:',mem)#'in:',infile,'yourout:',out.decode(),'correctout:',outfile)
+                if ret!=0:
+                        print('Result:Runtime Error')
+                        continue
+                if end-start>timer:
+                        print('Result:Time Limit Exceeded')
+                        continue
+                if mem >= int(data['memory']):
+                        print('Result:Memory Limit Exceeded')
+                        continue
+                if yourout != outfile:
+                        print('Result:Wrong Anwser')
+                        continue
+                print('Result:Accepted')
+                tot+=int(data['score'])
 	print('Total Score:',tot)
